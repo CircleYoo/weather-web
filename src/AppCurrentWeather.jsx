679e89b72f1bda1f1c6848f9583967900bdce6dc
JeongWon Yu <galleon32122@naver.com>
2023-02-14 14:14:33 +0900

    TT

---
+++ src/AppCurrentWeather.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import CurrentWeather from './components/CurrentWeather';
+
+export default function AppCurrentWeather() {
+    return (
+        <>
+            <CurrentWeather />
+        </>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppCurrentWeather from './AppCurrentWeather';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppCurrentWeather />
   </React.StrictMode>
 );
 
