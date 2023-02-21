af0601e53ca51fc0661b235e66f4216130aed8ed
JeongWon Yu <galleon32122@naver.com>
2023-02-21 18:06:31 +0900

    axios 5day.filter

--- src/components/CurrentLocationWeather.jsx
+++ src/components/CurrentLocationWeather.jsx
@@ -5,6 +5,7 @@ import "../App";
 export default function CurrentLocationWeather() {
   const [location, setLocation] = useState({ latitude: null, longitude: null });
   const [weather, setWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
   const API_KEY = "85949a76c886a99b91355b504b7e952e";
 
   useEffect(() => {
@@ -32,6 +33,21 @@ export default function CurrentLocationWeather() {
         });
     }
   }, [location]);
+
+  useEffect(() => {
+    if (location.latitude && location.longitude) {
+      axios
+        .get(`https://api.openweathermap.org/data/2.5/forecast?lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=${API_KEY}`)
+        .then((response) => {
+          setForecast(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [location]);
+
+  console.log(forecast.list.filter(reading => reading.dt_txt.includes("18:00:00")))
 
   return (
     <div>
